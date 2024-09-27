93551f11ce935722b21e55fad7042f5576724e53
Koustavjr <Koustavjr11@gmail.com>
2024-09-27 13:13:11 +0530

    basics

---
+++ 1-Basics/file1.py
@@ -0,0 +1,7 @@
+print("hello world")
+def print_n(n):
+    print(n)
+    
+print_n("Called from file 1")
+
+one = 'modified variable'
---
+++ 1-Basics/file2.py
@@ -0,0 +1,3 @@
+from file1 import print_n
+
+print_n("Called from file 2")
